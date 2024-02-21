9a398f03983b5694c43ac455f6023e2e7a81d1f9
CarHSMig <151793880+CarHSMig@users.noreply.github.com>
2024-02-20 21:46:16 -0300

    Create atividade1.java

---
+++ atividade1.java
@@ -0,0 +1,14 @@
+class Variaveis {
+    public static void main(String[] args) {
+        int quantidade = 25;
+        double pesoun = 50.5;
+        char genero = 'G';
+        boolean produtoemestoque = true;
+        short paraentrega = 27;
+        boolean faltamprodutos = true;
+        short quantidadefaltante = 2;
+        long codigodoproduto = 231087;
+        short quantidadesolicitada = 90;
+        float pesototal = 4545;
+    }
+}
