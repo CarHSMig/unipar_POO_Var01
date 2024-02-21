5ad0de217c93c31a57e71c51b792bf1cfc3a019d
CarHSMig <151793880+CarHSMig@users.noreply.github.com>
2024-02-20 22:00:22 -0300

    Create Atividade2.java

---
+++ Atividade2.java
@@ -0,0 +1,14 @@
+class Variaveis {
+    public static void main(String[] args) {
+    String nomeProduto
+    float precoProduto
+    String marcaDoProduto
+    int quantidadeProduto
+    String descricaoProduto
+    String nomeProduto
+    long numeroProduto    
+    float precoDoProduto
+    int estoqueDisponivel
+    int totaldeProdutos
+    }
+}
