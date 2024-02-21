1a5f6a6710fc0d507d87d8f140a4188cb28a7441
CarHSMig <151793880+CarHSMig@users.noreply.github.com>
2024-02-20 22:37:30 -0300

    Create Atividade7.java

---
+++ Atividade7.java
@@ -0,0 +1,16 @@
+public class ControleEstoque {
+    public static void main(String[] args) {
+
+        String nomeProduto = "PROCESSADOR";
+        double precoProduto = 29.99;
+        int quantidadeProduto = 10;
+        String marcaProduto = "Ryzen";
+        String nomeProduto1 = "Fone de Ouvido";
+        
+        System.out.println("Dados do Produto:");
+        System.out.println("Nome: " + nomeProduto1);
+        System.out.println("Marca: " + marcaProduto);
+        System.out.println("Preco: $" + precoProduto);
+        System.out.println("Quantidade em estoque: " + quantidadeProduto);
+    }
+}
