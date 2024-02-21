abdfd7a063ec7b3eb42e6a17e29f5c08da1e1c1b
CarHSMig <151793880+CarHSMig@users.noreply.github.com>
2024-02-20 22:28:07 -0300

    Create Atividade3.java

---
+++ Atividade3.java
@@ -0,0 +1,11 @@
+class Variaveis {
+    public static void main(String[] args) {
+    String nomdoAluno = "Rafael Sadhaack Stasiak Aspas Migon";
+    int IdadedoAluno = 23;
+    Double notadaProva1 = 10.0;
+    Double notadaProva2 = 2.0;
+    Double mediaFinal = ((notadaProva1 + notadaProva2)/2);
+    
+    System.out.println(mediaFinal);
+    }
+}
