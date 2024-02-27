3c7dcdc8f999a41fc8a3bae9314604eaf815a31d
CarHSMig <151793880+CarHSMig@users.noreply.github.com>
2024-02-26 22:27:52 -0300

    Create Atividade 4.java

---
+++ Atividade 4.java
@@ -0,0 +1,40 @@
+
+public class Main {
+    public static void main(String[] args) {
+        
+  
+        String nomeDoProdutoBebida = "Refri Kuait"; 
+        String categoriaDoProdutoBebida = "Bebida";
+        double precoDoProdutoBebida = 5.59; 
+        int quantidadeDoProdutoBebida = 200;
+
+        System.out.println("Nome: " + nomeDoProdutoBebida);
+        System.out.println("Marca: " + categoriaDoProdutoBebida);
+        System.out.println("Preco: " + precoDoProdutoBebida + "R$");
+        System.out.println("Quantidade: " + quantidadeDoProdutoBebida);
+        
+        System.out.println("");
+        
+        String nomeDoProdutoComida = "Macarrao"; 
+        String categoriaDoProdutoComida = "Comida";
+        double precoDoProdutoComida = 7.99; 
+        int quantidadeDoProdutoComida = 200;
+
+        System.out.println("Nome: " + nomeDoProdutoComida);
+        System.out.println("Marca: " + categoriaDoProdutoComida);
+        System.out.println("Preco: " + precoDoProdutoComida + "R$");
+        System.out.println("Quantidade: " + quantidadeDoProdutoComida);
+        
+         System.out.println("");
+        
+        String nomeDoProdutoHigiene = "Colgate pasta de dente"; 
+        String categoriaDoProdutoHigiene = "Bebida";
+        double precoDoProdutoHigiene = 3.59; 
+        int quantidadeDoProdutoHigiene = 200;
+
+        System.out.println("Nome: " + nomeDoProdutoHigiene);
+        System.out.println("Marca: " + categoriaDoProdutoHigiene);
+        System.out.println("Preco: " + precoDoProdutoHigiene + "R$");
+        System.out.println("Quantidade: " + quantidadeDoProdutoHigiene);
+    }
+}
