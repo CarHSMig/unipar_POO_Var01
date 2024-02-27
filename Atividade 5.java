3cb2763395c05a333d93e22afe635e65feec9336
CarHSMig <151793880+CarHSMig@users.noreply.github.com>
2024-02-26 21:33:16 -0300

    Create Atividade 5.java

---
+++ Atividade 5.java
@@ -0,0 +1,49 @@
+
+public class Main {
+    public static void main(String[] args) {
+        
+  
+        String nomeDoProdutoSmartphone = "Iphone 20 Pro Xyz"; 
+        String marcaDoProdutoSmartphone = "Iphome";
+        double precoDoProdutoSmartphone = 40000.00; 
+        int quantidadeDoProdutoSmartphone = 200;
+        String descricaoDoProdutoSmartphone = "Iphone 20 Pro Xyz de excelente qualidade";      
+
+        System.out.println("Dados do Smartphone:");
+        System.out.println("Nome: " + nomeDoProdutoSmartphone);
+        System.out.println("Marca: " + marcaDoProdutoSmartphone);
+        System.out.println("Preco: " + precoDoProdutoSmartphone + "R$");
+        System.out.println("Quantidade: " + quantidadeDoProdutoSmartphone);
+        System.out.println("Descricao: " + descricaoDoProdutoSmartphone);
+        
+        System.out.println("");
+        
+        String nomeDoProdutoLaptop = "Mac XVII"; 
+        String marcaDoProdutoLaptop = "Iphome";
+        double precoDoProdutoLaptop = 70689.90; 
+        int quantidadeDoProdutoLaptop = 400;
+        String descricaoDoProdutoLaptop = "Mac XVII de qualidade futurista";      
+
+        System.out.println("Dados do Laptop:");
+        System.out.println("Nome: " + nomeDoProdutoLaptop);
+        System.out.println("Marca: " + marcaDoProdutoLaptop);
+        System.out.println("Preco: " + precoDoProdutoLaptop + "R$");
+        System.out.println("Quantidade: " + quantidadeDoProdutoLaptop);
+        System.out.println("Descricao: " + descricaoDoProdutoLaptop);
+        
+         System.out.println("");
+        
+        String nomeDoProdutoFonedeOuvidoBluetooth = "AirBuds Pro 39"; 
+        String marcaDoProdutoFonedeOuvidoBluetooth = "Semsunga";
+        double precoDoProdutoFonedeOuvidoBluetooth = 15030.79; 
+        int quantidadeDoProdutoFonedeOuvidoBluetooth = 700;
+        String descricaoDoProdutoFonedeOuvidoBluetooth = "AirBuds Pro 39 pra ouvir o tustuts";      
+
+        System.out.println("Dados do Smartphone:");
+        System.out.println("Nome: " + nomeDoProdutoFonedeOuvidoBluetooth);
+        System.out.println("Marca: " + marcaDoProdutoFonedeOuvidoBluetooth);
+        System.out.println("Preco: " + precoDoProdutoFonedeOuvidoBluetooth + "R$");
+        System.out.println("Quantidade: " + quantidadeDoProdutoFonedeOuvidoBluetooth);
+        System.out.println("Descricao: " + descricaoDoProdutoFonedeOuvidoBluetooth);
+    }
+}
